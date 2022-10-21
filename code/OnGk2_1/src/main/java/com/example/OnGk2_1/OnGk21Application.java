13b7eb825db3fb142e2d0fb5869ad29bda3ce8fd
NguyenVVLap <nlap741@gmail.com>
2022-10-21 13:08:52 +0700

    finish Subject Service

---
+++ code/OnGk2_1/src/main/java/com/example/OnGk2_1/OnGk21Application.java
@@ -0,0 +1,13 @@
+package com.example.OnGk2_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnGk21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnGk21Application.class, args);
+	}
+
+}
